3cbe07cf02ddc86a2cf4c9655ca7f9a5454650fb
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-03-02 01:08:30 +0500

    add task68

---
+++ task68/Program.cs
@@ -0,0 +1,17 @@
+﻿int Ankerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Ankerman(m - 1, 1);
+    else 
+        return Ankerman(m - 1, Ankerman(m, n - 1));
+}
+
+Console.Clear();
+Console.Write("Введите первый параметр M функции Анкермана: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второй параметр N функции Анкермана: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Ankerman(M, N));
