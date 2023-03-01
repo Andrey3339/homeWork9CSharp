13b122359d65c1d0b6f138c4eb50cb77378d7315
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-03-02 01:07:29 +0500

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿int SumNumbers(int m, int n)
+{
+    if (n == m)
+        return m;
+    return SumNumbers(m, n -1) + n;
+}
+
+Console.Clear();
+Console.Write("Введите первое число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+while(N < M)
+{
+    Console.WriteLine("Второе число N должно быть больше или равно M.");
+    Console.Write("Введите второе число N: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(SumNumbers(M, N));
